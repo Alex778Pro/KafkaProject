4e7c002fb3344817ffa52845864a8981317874ae
Alex778Pro <aleksander.terehin2014@yandex.ru>
2025-08-03 10:44:27 +0300

    Продюсер второй задачи Kafka

---
+++ src/main/java/com/example/kafkaproject/KafkaProducerConfig.java
@@ -0,0 +1,47 @@
+package com.example.kafkaproject;
+
+import com.example.kafkaproject.entity.Order;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaProducerConfig {
+    @Bean
+    public ProducerFactory<String, Order> producerFactory(ObjectMapper objectMapper) {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 3);
+
+        JsonSerializer<Order> serializer = new JsonSerializer<>(objectMapper);
+        serializer.setAddTypeInfo(false);
+
+        return new DefaultKafkaProducerFactory<>(
+                props,
+                new StringSerializer(),
+                serializer
+        );
+    }
+
+    @Bean
+    public KafkaTemplate<String, Order> kafkaTemplate(
+            ProducerFactory<String, Order> producerFactory
+    ) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
